ed81581e65ffbc09e6c999ab039888680f038189
Xulong Zhang <soniczhangss@gmail.com>
2016-06-16 17:18:10 +1000

    added style on table

--- app/components/restaurant/restaurantDirective.js
+++ app/components/restaurant/restaurantDirective.js
@@ -1,5 +1,20 @@
 fxmenuAdminApp
+	.directive('fxmenuMouseoverChangeColor', MouseoverChangeColor );
+
+fxmenuAdminApp
 	.directive('fxmenuRestaurants', RestaurantDirective );
+
+function MouseoverChangeColor() {
+	return function($scope, $element){
+		$element.bind('mouseover', function() {
+			$($element).addClass('gray-color');
+		});
+
+		$element.bind('mouseleave', function() {
+			$($element).removeClass('gray-color');
+		});
+	};
+}
 
 function RestaurantDirective() {
 	return {
